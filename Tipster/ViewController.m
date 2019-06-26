ee0e31173b50162d2543d8d5c9e799fca44f6eeb
Ayooluwa Odemuyiwa <aodemuyi@fb.com>
2019-06-26 11:06:27 -0700

    Fixed small errors in tip calculator

--- Tipster/ViewController.m
+++ Tipster/ViewController.m
@@ -10,6 +10,11 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
+
 @end
 
 @implementation ViewController
@@ -18,6 +23,24 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
+
+- (IBAction)onEdit:(id)sender {
+    double bill = [self.billField.text doubleValue];
+    NSArray *percentages = @[@(0.15),@(0.2),@(0.22)];
+    double tipPercentage = [percentages[self.tipControl.selectedSegmentIndex]doubleValue];
+    double tip = tipPercentage * bill;
+    double total = bill + tip;
+    self.tipLabel.text =[NSString stringWithFormat: @"$%.2f",tip]; self.totalLabel.text=[NSString stringWithFormat: @"$%f",total];
+}
+- (IBAction)onEditingBegin:(id)sender {
+    self.billField.frame = CGRectMake(self.billField.frame.origin.x,self.billField.frame.origin.y,self.billField.frame.size.width, self.billField.frame.size.height);
+}
+
+- (IBAction)onEditingEnd:(id)sender {
+    CGRect newFrame = self.billField.frame;
+    newFrame.origin.y-=30;
+    self.billField.frame=newFrame;
+    }
 
 
 @end
